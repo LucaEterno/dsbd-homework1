bdf89fc016828cd391e8af8ea40f6fd63c6f0219
GrassoRosario <rosariograsso032@gmail.com>
2025-12-15 09:52:46 +0100

    Aggiunta Alert Notifier System

--- alert_notifier_system/notifier.py
+++ alert_notifier_system/notifier.py
@@ -1,33 +1,31 @@
-from confluent_kafka import Consumer, Producer, KafkaException, KafkaError
+from confluent_kafka import Consumer, KafkaException, KafkaError
 from confluent_kafka.admin import AdminClient
 import os
 import json
-from notification_logic import verification_logic
 import time
-from typing import List, Dict, Any
+from typing import Dict, List, Any, Optional
 from datetime import datetime
+import smtplib
+from email.message import EmailMessage
 
 # Configurazione Kafka
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:29092")
-TOPIC_TO_ALERT_SYSTEM="to-alert-system"
 TOPIC_TO_NOTIFIER="to-notifier"
+
+# Configurazione mailhog
+SMTP_SERVER = os.getenv("SMTP_SERVER", "mailhog")
+SMTP_PORT = os.getenv("SMTP_PORT", "1025")
+
 
 consumer_config = {
     'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS,
-    'group.id': 'group1',
+    'group.id': 'group2',
     'auto.offset.reset': 'earliest',
     'enable.auto.commit': False,
     'max.poll.interval.ms': 300000,
     'session.timeout.ms': 60000,
     "log_level": 3,
 }
-
-producer_config = {
-    'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS,
-    'retries': 5,
-    'linger.ms': 0,
-}
-
 
 def wait_for_kafka_and_topic():
     admin_conf = {
@@ -47,28 +45,22 @@ def wait_for_kafka_and_topic():
             print("Kafka Broker not ready, retrying in 5s...", flush=True)
             time.sleep(5)
 
-    # PARTE 2: Attesa che il topic sia pronto
-    print(f"Waiting for topic '{TOPIC_TO_ALERT_SYSTEM}' to be available (waiting for topic creator)...", flush=True)
+    # PARTE 2: Attesa che il Topic Creator abbia completato la creazione
+    print(f"Waiting for topic '{TOPIC_TO_NOTIFIER}' to be available (waiting for topic creator)...", flush=True)
     while True:
         try:
-            metadata = admin.list_topics(topic=TOPIC_TO_ALERT_SYSTEM, timeout=5)
+            metadata = admin.list_topics(topic=TOPIC_TO_NOTIFIER, timeout=5)
 
-            if TOPIC_TO_ALERT_SYSTEM in metadata.topics:
-                print(f"Topic '{TOPIC_TO_ALERT_SYSTEM}' is available.", flush=True)
+            if TOPIC_TO_NOTIFIER in metadata.topics:
+                print(f"Topic '{TOPIC_TO_NOTIFIER}' is available.", flush=True)
                 return
             else:
-                print(f"Topic '{TOPIC_TO_ALERT_SYSTEM}' not yet available. Retrying in 5s...", flush=True)
+                print(f"Topic '{TOPIC_TO_NOTIFIER}' not yet available. Retrying in 5s...", flush=True)
                 time.sleep(5)
 
         except Exception as e:
             print(f"Error checking topic status: {e}. Retrying in 5s...", flush=True)
             time.sleep(5)
-
-def delivery_report(err, msg):
-    if err:
-        print(f"Failed to produce to {TOPIC_TO_NOTIFIER}: {err}")
-    else:
-        print(f"Info sent to {TOPIC_TO_NOTIFIER} at offset {msg.offset()}")
 
 def format_notification(notification: Dict[str, Any]) -> str:
     """
@@ -113,35 +105,47 @@ def print_notifications(notifications: List[Dict[str, Any]]):
 
     print("="*40)
 
-def notification_producer(producer, results_to_notify):
+def sending_email(notifications):
     """
-    Invia l'elenco dei risultati di notifica al topic TOPIC_TO_NOTIFIER.
+    Invia le email
     """
-    if not results_to_notify:
+    # Stampa
+    if not notifications:
         print("Nessuna notifica da inviare.")
         return
 
-    print(f"Trovate {len(results_to_notify)} notifiche da inviare.")
-    print_notifications(results_to_notify)
+    print(f"Trovate {len(notifications)} notifiche da inviare.")
+    print_notifications(notifications)
 
-    for result in results_to_notify:
+    # Dati ricevuti da Kafka
+    for i, notification in enumerate(notifications):
+        ap_code = notification["airport_code"]
+        user_email = notification["user_email"]
+        condition = notification["condition"]
+
+        msg = EmailMessage()
+        msg.set_content(condition)
+        msg['Subject'] = ap_code
+        msg['To'] = user_email
+        msg['From'] = 'alert-system@your-app.com'
+
+        # Invio dell'email a MailHog
         try:
-            producer.produce(
-                TOPIC_TO_NOTIFIER,
-                key=result.get("user_email", "default_key").encode('utf-8'),
-                value=json.dumps(result).encode('utf-8'),
-                callback=delivery_report
-            )
-            producer.poll(0)
+            with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+                server.send_message(msg)
+            print("Email inviata con successo a MailHog.")
         except Exception as e:
-            print(f"Errore durante la produzione del messaggio: {e}")
+            print(f"Errore durante l'invio dell'email: {e}")
+
 
 def main():
     wait_for_kafka_and_topic()
     consumer = Consumer(consumer_config)
-    producer = Producer(producer_config)
-    consumer.subscribe([TOPIC_TO_ALERT_SYSTEM])
-    print(f"Alert System started")
+    consumer.subscribe([TOPIC_TO_NOTIFIER])
+    message_count = 0
+    BATCH_SIZE = 1
+    received_messages = []
+    print(f"Alert Notifier System started")
     try:
         while True:
             msg = consumer.poll(5.0)
@@ -159,30 +163,40 @@ def main():
             try:
                 #1. Messaggio ricevuto
                 data = json.loads(msg.value().decode('utf-8'))
-                ts = data['timestamp']
-                status = data['status']
-                print(f"Received message in to-alert-system topic. Timestamp = {ts}, Status = {status}")
+                received_messages.append(data)
+                message_count += 1
 
-                # 2. Logica di verifica
-                results_to_notify = verification_logic()
+                print(f"Received message in to-notifier topic.\n"
+                      f"Received message #{message_count} (batch progress: {message_count}/{BATCH_SIZE})")
 
-                #3. Invio delle notifiche
-                notification_producer(producer, results_to_notify)
+                # 2. Logica di invio mail
+                if message_count >= BATCH_SIZE:
+                    # Chiamata funzione che invia ogni mail con un for.
+                    sending_email(received_messages)
 
-                # 4. Commit Manuale
-                consumer.commit(message=msg, asynchronous=False)
-                print(f"Committed offset {msg.offset()}")
+                    consumer.commit(asynchronous=False)
+                    print(f"Committed offset: {msg.offset()}\n")
+
+                    # Reset for next batch
+                    received_messages = []
+                    message_count = 0
 
             except Exception as e:
                 print(f"Errore durante l'elaborazione: {e}")
+                consumer.commit(asynchronous=False)
                 continue
 
     except KeyboardInterrupt:
         print("\nConsumer-Producer interrupted by user.")
     finally:
         # Final cleanup
-        print("Flushing producer...")
-        producer.flush(timeout=10)
+        # Processa i messaggi rimanenti in buffer
+        if received_messages:
+            print("\nProcessing remaining messages before shutdown...")
+            # Chiamata alla funzione che invia mail con for
+            sending_email(received_messages)
+            consumer.commit(asynchronous=False)
+
         print("Closing consumer...")
         consumer.close()
         print("Shutdown complete")
